4722a66afb1817abb07c55431ab48528cdab163b
Bob Sheridan <djmousefarm@googlemail.com>
2016-10-17 16:43:40 +0100

    code is in it now :)

--- array_concat.js
+++ array_concat.js
@@ -1,1 +1,12 @@
 
+
+function diffArray(arr1, arr2) {
+  var newArr = arr1.concat(arr2);
+  newArr.sort();
+  
+  newArr = newArr.filter(function(e,i,a) {return e!==a[i+1] && a[i-1]!==e;});
+  // Same, same; but different.
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
